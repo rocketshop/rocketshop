8acb897ae14d3979e3b3e92f26b02b492e533dd8
Joakim Johansson <joakim.a.johansson@gmail.com>
2017-05-24 23:57:42 +0200

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+<div class="title m-b-md">
+    Rocket
+</div>
+
+<div class="links">
+    <a href="https://laravel.com/docs">A modern e-commerce solution</a>
+</div>
+@endsection
